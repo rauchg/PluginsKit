63bcabe021c7f5c6a9c6037deda85ffbd3338d24
Guillermo Rauch <rauchg@gmail.com>
2009-11-23 10:48:48 -0800

    Cache clear fix Images relative paths fix

--- lib/vendor/symfony/lib/task/cache/sfCacheClearTask.class.php
+++ lib/vendor/symfony/lib/task/cache/sfCacheClearTask.class.php
@@ -78,7 +78,7 @@ EOF;
     }
 
     // finder to find directories (1 level) in a directory
-    $dirFinder = sfFinder::type('dir')->discard('.sf')->discard('.gitignore')->maxdepth(0)->relative();
+    $dirFinder = sfFinder::type('dir')->discard('.sf')->maxdepth(0)->relative();
 
     // iterate through applications
     $apps = is_null($options['app']) ? $dirFinder->in(sfConfig::get('sf_apps_dir')) : array($options['app']);
@@ -125,7 +125,7 @@ EOF;
     // clear global cache
     if (is_null($options['app']) && 'all' == $options['type'])
     {
-      $this->getFilesystem()->remove(sfFinder::type('file')->discard('.sf')->in(sfConfig::get('sf_cache_dir')));
+      $this->getFilesystem()->remove(sfFinder::type('file')->discard('.gitignore')->discard('.sf')->in(sfConfig::get('sf_cache_dir')));
     }
   }
 
